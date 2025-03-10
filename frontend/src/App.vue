363599f30f01a0f6a9cae43c307b11fc461d6c24
Ivan Kraev <ivankraev122@gmail.com>
2025-03-10 23:13:32 +0200

    updated ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,8 @@ import userStore from './pages/user/user.store';
 const recordName = ref('');
 const records = computed(() => userStore.listRecords().value);
 
-const createRecord = async () => {
+const saveRecord = async () => {
+  if (recordName.value === '') return;
   userStore.save({ name: recordName.value, email: 'test@gmail.com' });
   recordName.value = '';
 };
@@ -19,15 +20,28 @@ onUnmounted(() => userStore.destroy());
 </script>
 
 <template>
-  <h5>Users:</h5>
-  <ul>
-    <li v-for="record in records" :key="record.id">
-      {{ record.name }}
-      <button @click="removeRecord(record.id)">Delete</button>
-    </li>
-  </ul>
-  <form @submit.prevent="createRecord">
-    <input v-model="recordName" placeholder="Enter new record" />
-    <button type="submit">Create</button>
-  </form>
+  <div class="flex flex-center" style="height: 100vh">
+    <q-card style="width: 500px">
+      <q-card-section>
+        <q-toolbar>
+          <q-toolbar-title>Users ({{ records.length }})</q-toolbar-title>
+        </q-toolbar>
+        <q-list bordered v-if="records.length" separator>
+          <q-item v-for="record in records" :key="record.id">
+            <q-item-section>{{ record.name }}</q-item-section>
+            <q-item-section side>
+              <q-btn flat round icon="delete" @click="removeRecord(record.id)" color="negative" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+
+      <q-card-section>
+        <q-form @submit.prevent="saveRecord">
+          <q-input v-model="recordName" placeholder="Enter user name" />
+          <q-btn type="submit" color="primary" label="Create" class="q-mt-md" />
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
